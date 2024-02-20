b0735e788423e0df918673d336a0538e040bf1cc
NathanCo2 <156120309+NathanCo2@users.noreply.github.com>
2024-02-20 14:03:33 -0800

    Updated

--- src/basic_tasks_example.py
+++ src/basic_tasks_example.py
@@ -14,80 +14,67 @@ import gc
 import pyb
 import cotask
 import task_share
-from motor_driver import MotorDriver
-from encoder_reader import Encoder
-from motor_controller_Nathan import MotorController
-import utime
 
 
-def task1_fun(setpoint, gain):
+def task1_fun(shares):
     """!
-    Task which runs motor 1. Initiliazes then calls run()
-    @param setpoint Defines the desired setpoint of the controller
-    @param gain Defines the proportional gain of the controller
+    Task which puts things into a share and a queue.
+    @param shares A list holding the share and queue used by this task
     """
-    
-    # Initialize motor drivers and encoders
-    # set up timer 8 for encoder 2
-    TIM8 = pyb.Timer(8, prescaler=1, period=0xFFFF) # Timer 8, no prescalar, frequency 100kHz
-    #Define pin assignments for encoder 2
-    pinc6 = pyb.Pin(pyb.Pin.board.PC6)
-    pinc7 = pyb.Pin(pyb.Pin.board.PC7)
-    # Create encoder object
-    Jerry = Encoder(pinc6, pinc7, TIM8)
-    # setup motor
-    TIM5 = pyb.Timer(5, freq=2000) # Timer 5, frequency 2000Hz
-    # Define pin assignments for motor 2
-    pinc1 = pyb.Pin(pyb.Pin.board.PC1, pyb.Pin.OUT_PP)
-    pina0= pyb.Pin(pyb.Pin.board.PA0)
-    pina1 = pyb.Pin(pyb.Pin.board.PA1)    
-    # Create motor driver
-    Tom = MotorDriver(pinc1, pina0, pina1, TIM5)
-    Jerry.zero()
-    # Create motor controller
-    Deitch = MotorController(gain, setpoint, Tom.set_duty_cycle, Jerry.read)
-    
+    # Get references to the share and queue which have been passed to this task
+    my_share, my_queue = shares
+
+    counter = 0
     while True:
-        Deitch.run()
-        yield
+        my_share.put(counter)
+        my_queue.put(counter)
+        counter += 1
+
+        yield 0
 
 
-def task2_fun(setpoint, gain):
+def task2_fun(shares):
     """!
-    Task which runs motor 2. Initializes then calls run()
-    @param setpoint Defines the desired setpoint of the controller
-    @param gain Defines the proportional gain of the controller
+    Task which takes things out of a queue and share and displays them.
+    @param shares A tuple of a share and queue from which this task gets data
     """
     # Get references to the share and queue which have been passed to this task
     the_share, the_queue = shares
 
     while True:
+        # Show everything currently in the queue and the value in the share
+        print(f"Share: {the_share.get ()}, Queue: ", end='')
+        while q0.any():
+            print(f"{the_queue.get ()} ", end='')
+        print('')
 
-        yield
+        yield 0
 
 
 # This code creates a share, a queue, and two tasks, then starts the tasks. The
 # tasks run until somebody presses ENTER, at which time the scheduler stops and
 # printouts show diagnostic information about the tasks, share, and queue.
 if __name__ == "__main__":
-    print("Testing two motor at once"
+    print("Testing ME405 stuff in cotask.py and task_share.py\r\n"
           "Press Ctrl-C to stop and show diagnostics.")
+
+    # Create a share and a queue to test function and diagnostic printouts
+    share0 = task_share.Share('h', thread_protect=False, name="Share 0")
+    # Creates a shared signed short integer (16 bit: -32780 to 32767) that is
+    # not thread protected (multiple taks can access at once) and named for debugging
     
+    q0 = task_share.Queue('L', 16, thread_protect=False, overwrite=False,
+                          name="Queue 0")
+    # Creates a shared queue with a max capacity of 16 that holds unsigned long i
+
     # Create the tasks. If trace is enabled for any task, memory will be
     # allocated for state transition tracing, and the application will run out
     # of memory after a while and quit. Therefore, use tracing only for 
     # debugging and set trace to False when it's not needed
-    
-    gain1 = 0.2
-    setpoint1 = 36000
-    
-    gain2 = 0.2
-    setpoint2 = -36000
-    
     task1 = cotask.Task(task1_fun, name="Task_1", priority=1, period=400,
-                        profile=True, trace=False, gain1, setpoint1)
+                        profile=True, trace=False, shares=(share0, q0))
     task2 = cotask.Task(task2_fun, name="Task_2", priority=2, period=1500,
-                        profile=True, trace=False, gain2, setpoint2)
+                        profile=True, trace=False, shares=(share0, q0))
     cotask.task_list.append(task1)
     cotask.task_list.append(task2)
 
@@ -101,7 +88,7 @@ if __name__ == "__main__":
             cotask.task_list.pri_sched()
         except KeyboardInterrupt:
             break
-    
+
     # Print a table of task data and a table of shared information data
     print('\n' + str (cotask.task_list))
     print(task_share.show_all())
