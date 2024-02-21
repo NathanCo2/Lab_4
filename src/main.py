45eecb5123b486bbc055aab1d73cfff16a12ac17
jpere180 <156122419+jpere180@users.noreply.github.com>
2024-02-21 15:16:18 -0800

    3:16

--- src/main.py
+++ src/main.py
@@ -35,6 +35,7 @@ def task1_fun(setpoint, gain):
     pinc7 = pyb.Pin(pyb.Pin.board.PC7)
     # Create encoder object
     Jerry = Encoder(pinc6, pinc7, TIM8)
+    
     # setup motor
     TIM5 = pyb.Timer(5, freq=2000) # Timer 5, frequency 2000Hz
     # Define pin assignments for motor 2
@@ -51,7 +52,6 @@ def task1_fun(setpoint, gain):
         Deitch.run()
         yield
 
-
 def task2_fun(setpoint, gain):
     """!
     Task which runs motor 2. Initializes then calls run()
@@ -60,11 +60,31 @@ def task2_fun(setpoint, gain):
     """
     # Get references to the share and queue which have been passed to this task
     the_share, the_queue = shares
-
+    
+    # Initialize motor drivers and encoders
+    # Set up timer 4 for encoder 1
+    TIM4 = pyb.Timer(4, prescaler=1, period=0xFFFF) # Timer 4, no prescalar, frequency 100kHz
+    # Define pin assignments for encoder 1
+    pinb6 = pyb.Pin(pyb.Pin.board.PB6)
+    pinb7 = pyb.Pin(pyb.Pin.board.PB7)
+    # Create encoder object
+    Jess = Encoder(pinb6, pinb7, TIM4)
+    
+    # setup motor
+    TIM3 = pyb.Timer(3, freq=2000) # Timer 3, frequency 2000Hz
+    # Define pin assignments for motor 1
+    pina10 = pyb.Pin(pyb.Pin.board.PA10, pyb.Pin.OUT_PP)
+    pinb4= pyb.Pin(pyb.Pin.board.PB4)
+    pinb5 = pyb.Pin(pyb.Pin.board.PB5)
+    # Create motor driver
+    Jackie = MotorDriver(pina10, pinb4, pinb5, TIM3)
+    Jess.zero()
+    # Create motor controller
+    AA = MotorController(gain, setpoint, Jackie.set_duty_cycle, Jess.read)
+    
     while True:
-
+        AA.run()
         yield
-
 
 # This code creates a share, a queue, and two tasks, then starts the tasks. The
 # tasks run until somebody presses ENTER, at which time the scheduler stops and
@@ -80,9 +100,11 @@ if __name__ == "__main__":
     
     gain1 = 0.2
     setpoint1 = 36000
+    my_share.put(gain1, setpoint1)
     
     gain2 = 0.2
     setpoint2 = -36000
+    my_share.put(gain2, setpoint2)
     
     task1 = cotask.Task(task1_fun, name="Task_1", priority=1, period=400,
                         profile=True, trace=False, gain1, setpoint1)
